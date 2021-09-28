3be8c0dc042915b357e449d61e613a86196f85b6
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-09-28 15:38:39 +0530

    added starter files for templating

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.use("/admin", adminRoutes);
 app.use("/", shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000);
