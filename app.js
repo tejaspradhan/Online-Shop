4eeabca58f29a63b4d86cc2ff151c0614f7b533f
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-09-28 11:27:12 +0530

    completed path configurations for html pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const express = require("express");
+const path = require("path");
 const { urlencoded } = require("body-parser");
 const app = express();
 
@@ -8,11 +9,9 @@ const shopRoutes = require("./routes/shop");
 
 app.use(urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
-app.use("/shop", shopRoutes);
+app.use("/", shopRoutes);
 
 app.use((req, res) => {
-  res.status(404).send(`
-  <h1>Page Not Found</h1>
-  `);
+  res.status(404).sendFile(path.join(__dirname, "views", "error.html"));
 });
 app.listen(3000);
