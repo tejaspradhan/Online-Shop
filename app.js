0effb0d47e02c8549c818d6fd7d5c97308977100
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-09-30 13:06:02 +0530

    completed ejs templating

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const http = require("http");
-const express = require("express");
-const path = require("path");
-const { urlencoded } = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminData = require("./routes/admin.js");
-const shopRoutes = require("./routes/shop");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.set('view engine', 'pug');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));  // for css 
-app.use("/admin", adminData.routes);
-app.use("/", shopRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res) => {
-  res.status(404).render('404',{pageTitle : 'Error'});
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
+
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,22 @@
-const express = require("express");
-const path = require('path')
-const router = express.Router();
-const adminData = require("./admin.js");
+const path = require('path');
 
-router.get("/", (req, res, next) => {
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop.pug',{prods: products, docTitle:'Shop'});
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
